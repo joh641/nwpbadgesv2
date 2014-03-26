435def275d055d7d8c44c75306e4cc1f752c048b
Joshua Lam <joh641@gmail.com>
2014-03-26 15:48:36 -0700

    root as env var

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,8 @@ require File.expand_path('../application', __FILE__)
 environment_variables = File.join(Rails.root, 'config', 'environment_variables.rb')
 load(environment_variables) if File.exists?(environment_variables)
 
+ENV['root'] = 'http://badge.nwp.org'
+
 # Initialize the rails application
 Nwpbadges::Application.initialize!
 
@@ -13,6 +15,6 @@ ActionMailer::Base.smtp_settings = {
   :authentication => :plain,
   :user_name      => ENV['SENDGRID_USERNAME'],
   :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com',
+  :domain         => 'nwp.org',
   :enable_starttls_auto => true
 }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,6 @@ Nwpbadges::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'nwpbadges.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => "#{ENV['root']}" }
 
 end
